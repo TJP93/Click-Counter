ea29e9855366cebe4c6658557b4fe89338fc2e0e
Tom Porter <tom.port@yahoo.co.uk>
2022-01-28 13:43:11 +0000

    counter function written and HTML cleaned up. Project working as intended.

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+let count = 0;
 
+const counter = document.querySelector('#counter')
+const buttons = document.querySelectorAll('.btn')
+
+buttons.forEach(function (button){
+ button.addEventListener('click', function(e) {
+     const styles = e.currentTarget.classList;
+     if(styles.contains('increase')) {
+         count++;
+     } 
+     else if (styles.contains('decrease')) {
+         count--;
+     }
+     else {
+         count = 0;
+     }
+     if(count > 0) {
+         counter.style.color = 'green';
+     }
+     if(count < 0) {
+         counter.style.color = 'red';
+     }
+     if (count === 0) {
+         counter.style.color = 'black';
+     }
+     counter.textContent = count;
+     });
+ });
